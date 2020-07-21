b77a2e6e56d518dd9662562ed74c8709874537da
Alf <alfcrisci@gmail.com>
2020-07-21 11:18:13 +0200

    update

---
+++ js/munapbatgame.js
@@ -0,0 +1,45 @@
+var species=['../images/barbastella_barbastellus.jpg',
+             '../images/hypsugo_savii.jpg',
+             '../images/myotis_daubentonii.jpg',
+             '../images/myotis_myotis.jpg',
+             '../images/nyctalus_noctula.jpg',
+             '../images/pipistrellus kuhlii.jpg',
+             '../images/rhinolophus_ferrumequinum.jpg',
+             '../images/rhinolophus_hipposideros.jpg',
+             '../images/tadarida_teniotis.jpg']
+
+var sounds= ['../sounds/barbastella_barbastellus.mp3',
+             '../sounds/hypsugo_savii.mp3',
+             '../sounds/myotis_daubentonii.mp3',
+             '../sounds/myotis_myotis.mp3',
+             '../sounds/nyctalus_noctula.mp3',
+             '../sounds/pipistrellus_kuhlii.mp3',
+             '../sounds/rhinolophus_ferrumequinum.mp3',
+             '../sounds/rhinolophus_hipposideros.mp3',
+             '../sounds/tadarida_teniotis.mp3']
+
+var diplom= ['images/d5.jpg',
+             'images/d6.jpg',
+             'images/d7.jpg',
+             'images/d8.jpg',]
+
+var speciesd=['images/barbastella_barbastellus.jpg',
+             'images/hypsugo_savii.jpg',
+             'images/myotis_daubentonii.jpg',
+             'images/myotis_myotis.jpg',
+             'images/nyctalus_noctula.jpg',
+             'images/pipistrellus kuhlii.jpg',
+             'images/rhinolophus_ferrumequinum.jpg',
+             'images/rhinolophus_hipposideros.jpg',
+             'images/tadarida_teniotis.jpg']
+
+var soundsd= ['sounds/barbastella_barbastellus.mp3',
+             'sounds/hypsugo_savii.mp3',
+             'sounds/myotis_daubentonii.mp3',
+             'sounds/myotis_myotis.mp3',
+             'sounds/nyctalus_noctula.mp3',
+             'sounds/pipistrellus kuhlii.mp3',
+             'sounds/rhinolophus_ferrumequinum.mp3',
+             'sounds/rhinolophus_hipposideros.mp3',
+             'sounds/tadarida_teniotis.mp3']
+
